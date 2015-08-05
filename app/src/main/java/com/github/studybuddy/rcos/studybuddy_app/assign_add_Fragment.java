b207e049834fedb77ae74e462a9006d160c042c8
MarinaEspinoza <espinm3@rpi.edu>
2015-08-05 16:07:31 -0400

    created new layout for Add Assignment Page

--- app/src/main/java/com/github/studybuddy/rcos/studybuddy_app/assign_add_Fragment.java
+++ app/src/main/java/com/github/studybuddy/rcos/studybuddy_app/assign_add_Fragment.java
@@ -6,11 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.github.studybuddy.rcos.studybuddy_app.Planner.Assignment;
+
 /**
  * Created by marina on 8/3/15.
  */
 public class assign_add_Fragment extends Fragment {
     View rootview;
+    private Assignment new_assign;
+    private String title;
+    private String course;
+    private String points;
+    private String date;
+    private String descript;
 
     @Nullable
     @Override
@@ -18,6 +30,27 @@ public class assign_add_Fragment extends Fragment {
         rootview = inflater.inflate(R.layout.add_assign_layout, container, false);
         return rootview;
     }
+
+//    public void onStart(){
+//        super.onStart();
+//        Button done_button = (Button) rootview.findViewById(R.id.button_done);
+//        done_button.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                EditText title_txt = (EditText) rootview.findViewById(R.id.assign_title_input);
+//                title = title_txt.toString();
+//
+//                EditText course_txt = (EditText) rootview.findViewById(R.id.assign_class_input);
+//                course = course_txt.toString();
+//
+//                EditText descript_txt = (EditText) rootview.findViewById(R.id.assign_descript_input);
+//                descript = descript_txt.toString();
+//
+//
+//            }
+//        });
+//    }
 
 
 
